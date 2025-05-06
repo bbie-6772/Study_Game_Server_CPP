da215d97f976241c97dae225fb4919e032bcd26c
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-05-06 11:45:12 +0900

    fix: 중복 참조 제거

--- ChattingServer/ServerNetLib/interface_tcp_network.h
+++ ChattingServer/ServerNetLib/interface_tcp_network.h
@@ -1,3 +1,34 @@
 #pragma once
 
-#include
+#include "define.h"
+#include "server_network_error_code.h"
+#include "interface_log.h"
+
+namespace NServerNetLib
+{
+	class ITcpNetwork
+	{
+	public:
+		ITcpNetwork() {}
+
+		virtual ~ITcpNetwork() {}
+
+		virtual NET_ERROR_CODE Init(const ServerConfig* pConfig, ILog* pLogger) { return NET_ERROR_CODE::kNONE;  }
+
+		virtual NET_ERROR_CODE SendData(const int32_t sessionIndex, const int16_t packetId,
+			const int16_t size, const unsigned char* pMsg) {
+			return NET_ERROR_CODE::kNONE;
+		}
+
+		virtual void Run() {}
+
+		virtual void Release() {}
+
+		virtual void ForcingClose(const int32_t sessionIndex) {}
+
+		virtual int32_t ClientSessionPoolSize() { return 0;  }
+
+		virtual RecvPacketInfo GetPacketInfo() { return RecvPacketInfo(); }
+
+	};
+}
